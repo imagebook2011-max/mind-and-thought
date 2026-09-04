573a2516e1e80ddd272d1c3f6ee8d0025e255a37
imagebook2011-max <imagebook2011@gmail.com>
2026-09-04 14:53:36 +0900

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,1 +1,25 @@
-module.exports = function (eleventyConfig) { eleventyConfig.addPassthroughCopy("src/css"); eleventyConfig.addCollection("posts", function (collectionApi) { return collectionApi.getFilteredByGlob("src/posts/*.md").sort((a, b) => { return b.date - a.date; }); }); eleventyConfig.addFilter("koreanDate", function (dateObj) { const d = new Date(dateObj); return `${d.getFullYear()}.${String(d.getMonth() + 1).padStart(2, "0")}.${String( d.getDate() ).padStart(2, "0")}`; }); return { dir: { input: "src", output: "_site", includes: "_includes", }, }; };
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/images");
+
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/posts/*.md").sort((a, b) => {
+      return b.date - a.date;
+    });
+  });
+
+  eleventyConfig.addFilter("koreanDate", function (dateObj) {
+    const d = new Date(dateObj);
+    return `${d.getFullYear()}.${String(d.getMonth() + 1).padStart(2, "0")}.${String(
+      d.getDate()
+    ).padStart(2, "0")}`;
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+    },
+  };
+};
